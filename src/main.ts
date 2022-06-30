73583e879de3e40ff19806ec14866ed992a320f4
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-06-30 16:37:13 -0300

    Correção

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { ValidationPipe } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
@@ -6,6 +6,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 5000);
+  await app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+    Logger.log('Server running...');
+  });
 }
 bootstrap();
