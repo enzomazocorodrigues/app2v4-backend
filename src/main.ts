0deebea2cd61da23f6f8816e9dc8f3fdf5493a61
enzomazocorodrigues <enzo.rodrigues2@gmail.com>
2022-07-01 14:17:19 -0300

    Correção

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 5000, '0.0.0.0', () => {
+
+  const HOST = process.env.HOST || '0.0.0.0';
+  const PORT = parseInt(process.env.PORT) || 5000;
+
+  await app.listen(PORT, HOST, () => {
     Logger.log('Server running...');
   });
 }
